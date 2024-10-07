c5f7cca758f67c1fab5f5d465b5262dba11f4eff
Aruvlte <1787942785@qq.com>
2024-10-07 10:41:14 +0800

    Initial commit

---
+++ build/modules/assembly1/CMakeFiles/assembly1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assembly1.dir/src/assembly1.cpp.o"
+  "CMakeFiles/assembly1.dir/src/assembly1.cpp.o.d"
+  "CMakeFiles/assembly1.dir/test/assembly1_test.cpp.o"
+  "CMakeFiles/assembly1.dir/test/assembly1_test.cpp.o.d"
+  "libassembly1.pdb"
+  "libassembly1.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assembly1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/modules/assembly2/CMakeFiles/assembly2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assembly2.dir/src/assembly2.cpp.o"
+  "CMakeFiles/assembly2.dir/src/assembly2.cpp.o.d"
+  "libassembly2.pdb"
+  "libassembly2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assembly2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
